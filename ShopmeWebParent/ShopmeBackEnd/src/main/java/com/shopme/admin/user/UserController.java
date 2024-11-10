0381c8584b7b0bd0a84bc6e072be5231bcad583e
Jie <kat_is_tiger@outlook.com>
2024-11-10 08:01:55 -0800

    code filter function for user Lists

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -27,12 +27,15 @@ public class UserController {
 
     @GetMapping("/users")
     public String listFirstPage(Model model) {
-        return listByPage(1, model,"firstName","asc");
+        return listByPage(1, model,"firstName","asc",null);
     }
 
     @GetMapping("/users/page/{pageNum}")
-    public String listByPage(@PathVariable(name = "pageNum") int pageNum, Model model, @Param("sortField") String sortField,@Param("sortDir") String sortDir) {
-        Page<User> page = service.listByPage(pageNum,sortField,sortDir);
+    public String listByPage(@PathVariable(name = "pageNum") int pageNum, Model model,
+                             @RequestParam("sortField") String sortField,
+                             @RequestParam("sortDir") String sortDir,
+                             @RequestParam("keyword") String keyword) {
+        Page<User> page = service.listByPage(pageNum,sortField,sortDir,keyword);
         List<User> listUsers = page.getContent();
 
         long startCount = (pageNum - 1) * UserService.USERS_PER_PAGE + 1;
@@ -50,6 +53,7 @@ public class UserController {
         model.addAttribute("sortField",sortField);
         model.addAttribute("sortDir",sortDir);
         model.addAttribute("reverseSortDir",reverseSortDir);
+        model.addAttribute("keyword",keyword);
         return "users";
     }
     @GetMapping("/users/new")
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRepository.java
@@ -1,11 +1,15 @@
 package com.shopme.admin.user;
 
 import com.shopme.common.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.domain.Page;
+
 
 public interface UserRepository extends JpaRepository<User,Integer> {
     @Query("SELECT u FROM User u WHERE u.email=:email")
@@ -13,6 +17,8 @@ public interface UserRepository extends JpaRepository<User,Integer> {
 
 
     public Long countById( Integer id);
+    @Query("select u from User u where concat(u.id,' ',u.email,' ',u.firstName,' ',u.lastName,' ') like %?1%")
+    public Page<User> findAll(String keyword, Pageable pageable);
 
 //    @Query("UPDATE User u SET u.enabled = ?2 WHERE u.id = ?1")
 //
